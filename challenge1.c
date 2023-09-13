3fb836730e5a6ba3246b862fc4ec372059651029
abdeltahaali@gmail.com <abdeltahaali@gmail.com>
2023-09-13 17:46:54 +0100

    Ajout de Readme et challenge1 program

---
+++ challenge1.c
@@ -0,0 +1,27 @@
+:#include <stdio.h>
+
+int main(void)
+{
+	char firstname[55];
+	char lastname[55];
+	int age;
+	char genre[55];
+	long number;
+	
+	printf("Entrer ton nom: ");
+	scanf("%s", firstname);
+	printf("Entrer ton prénom: ");
+	scanf("%s", lastname);
+	printf("Entrer ton age: ");
+	scanf("%d", &age);
+	printf("Entrer le sexe: ");
+	scanf("%s", genre);
+	printf("Entrer numero: ");
+	scanf("%ld", &number);
+
+	printf("Nom: %s\n", firstname);
+	printf("Prenom: %s\n", lastname);
+	printf("Age: %d\n", age);
+	printf("Sexe: %s\n", genre);
+	printf("Numero: %ld\n", number);
+}
