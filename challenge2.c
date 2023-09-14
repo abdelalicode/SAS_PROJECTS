78ae9272cbe92165996881b71659adf70e21a8c5
co detoyas <codetoyas@gmail.com>
2023-09-14 11:44:27 +0100

    Modification de resultat format float

--- challenge2.c
+++ challenge2.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-	int F, C;
+	float F, C;
 
-	printf("Entrer la temperature: ");
-	scanf("%d", &F);
+	printf("Entrer la temperature en Celsius: ");
+	scanf("%f", &F);
 
 	C = (F-32)/1.8;
 
-	printf("%d°C\n" , C);
+	printf("%.2f°C\n" , C);
 	
 	if (C > 35)
 	{
