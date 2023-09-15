e597949675da80ec720cafb4e8640893e11d2668
co detoyas <codetoyas@gmail.com>
2023-09-15 01:15:55 +0100

    Fixer l'incorrect resultat des secondes

--- Part2/challenge5.c
+++ Part2/challenge5.c
@@ -8,13 +8,13 @@ int main(void)
 	printf("Entrer l'année:\n");
 	scanf("%d", &an);
 
-	secs = an * 365 * 24 * 60 * 60;
+	secs = (long long)an * 365 * 24 * 60 * 60;
 	 
-	printf("les mois equivalents: %d\n", an * 12);
-	printf("les jours equivalents: %d\n", an * 365);
-	printf("les heures equivalents: %d\n", an * 365 * 24);
-	printf("les minutes equivalents: %d\n", an * 365 * 24 * 60);
-	printf("les secondes equivalents: %lld\n", secs);
+	printf("les mois equivalents: %d mois\n" , an * 12);
+	printf("les jours equivalents: %d jours\n", an * 365);
+	printf("les heures equivalents: %d heures\n", an * 365 * 24);
+	printf("les minutes equivalents: %d minutes\n", an * 365 * 24 * 60);
+	printf("les secondes equivalents: %lld secondes\n", secs);
 
 	return 0;
 }
